f6db0a41177bf5710dd7bb4e6c53328df9c7874e
Samuel Adams <sama29571@gmail.com>
2024-11-29 10:57:30 +0000

    connect to cluster

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-const express=require('express')
-const app=express()
-const print=console.log
-const mongoose=require('mongoose')
-const morgan=require('morgan')
-const helmet=require('helmet')
-const dotenv=require('dotenv')
-dotenv.config()
+const express = require("express");
+const app = express();
+const print = console.log;
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose
+  .connect(process.env.CONNECTION_STRING)
+  .then(() => {
+    print("connection successful");
+  })
+  .catch((error) => {
+    print("error don come");
+    print(error);
+  });
 
 
-mongoose.connect(process.env.CONNECTION_STRING).then(()=>{
-    print('connection successful')
-}).catch(
-    (error)=>
-        {
-            print('error don come')
-            print(error)
-        })
+app.use(express.json())
+app.use(helmet())
 
 
-app.listen(3000,'0.0.0.0',()=>{
-print('server up and running')
-})
+
+app.listen(3000, "0.0.0.0", () => {
+  print("server up and running");
+});
